2042e2148e4ebad55f4a2a4409825cfbef4b38ea
luis <lsaborit@uclv.cu>
2021-11-09 11:38:20 -0500

    converted euler tour code into template form

--- code/graphs/euler.cpp
+++ code/graphs/euler.cpp
@@ -1,34 +1,33 @@
-/*  Euler Tour  
-    - Un grafo conexo admite un ciclo euleriano ssi todos sus vertices 
-      tienen grado par, especificamente un grafo dirigido debe tener en 
-      cada nodo un grado de salida igual al grado de entrada.  
-    - Un grafo conexo admite un camino euleriano ssi todos sus vertices 
-      tienen grado par, excepto dos: el origen y el final del camino. 
-      Especificamente, un grafo dirigido debe tener en cada nodo un grado 
-      de salida igual al grado de entrada, excepto el origen con el grado 
-      de salida mayor en uno, y el final con grado de entrada mayor en 
-      uno. 
-    - El algoritmo se usa para hallar el ciclo euleriano. Si se desea 
-      hallar un camino euleriano, hay que colocar la arista faltante y 
-      luego borrarla del ciclo para obtener el camino.  
-    
-    - Complexity: O(E) 
-    - Tested on: http://poj.org/problem?id=2230
+/* Euler Tour  
+ - Un grafo conexo admite un ciclo euleriano ssi todos sus vertices 
+   tienen grado par, especificamente un grafo dirigido debe tener en 
+   cada nodo un grado de salida igual al grado de entrada.  
+ - Un grafo conexo admite un camino euleriano ssi todos sus vertices 
+   tienen grado par, excepto dos: el origen y el final del camino. 
+   Especificamente, un grafo dirigido debe tener en cada nodo un
+   grado de salida igual al grado de entrada, excepto el origen con
+   el grado de salida mayor en uno, y el final con grado de entrada
+   mayor en uno. 
+ - El algoritmo se usa para hallar el ciclo euleriano. Si se desea
+   hallar un camino euleriano, hay que colocar la arista faltante y
+   luego borrarla del ciclo para obtener el camino.  
+ - Complexity: O(E) 
+ - Tested on: http://codeforces.com/contest/1511/problem/D
 */
-struct par{
+struct edge{
     int nwn;
     bool mk;
 };
 
-int cn, ca;
-vector<par> G[MX];
-vector<int> sol;
-
-void euler(int nod){
-    for(int i=0; i < (int)G[nod].size(); i++) if( !G[nod][i].mk ){
-        G[nod][i].mk = true;
-        
-        euler(G[nod][i].nwn);
-    }
-    sol.pb(nod);
+vector<int> euler_tour(int ni, vector<edge> G[]){
+    vector<int> sol;
+    
+    function<void(int)> dfs = [&](int nod){
+        for(auto& i: G[nod]) if( !i.mk ){
+            i.mk = true;
+            dfs(i.nwn);
+        }
+        sol.pb(nod);
+    };
+    dfs(ni); return sol;
 }
