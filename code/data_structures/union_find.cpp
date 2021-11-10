0112cec5e7512ddd52a3ec5eab451bdad3e8d318
luis <lsaborit@uclv.cu>
2021-11-09 20:45:47 -0500

    memory consumption of union_find made O(n)

--- code/data_structures/union_find.cpp
+++ code/data_structures/union_find.cpp
@@ -5,20 +5,20 @@
 
 struct ds{
     int N;
-    int SET[MX], R[MX];
-    
-    ds(int N): N(N){
+    vector<int> SET, R;
+
+    ds(int N): N(N), SET(N+1), R(N+1){
         for(int i=1; i<=N; i++)
             SET[i] = i, R[i] = 1;
     }
-    
+
     int fset(int v){ return SET[v] != v ? SET[v] = fset(SET[v]) : v; }
     inline bool diff(int a, int b){ return fset(a) != fset(b); }
-    
+
     bool join(int a, int b){
         if( (a = fset(a)) == (b = fset(b)) )
             return false;
-         
+
         if( R[a] < R[b] ) swap(a, b);
         SET[b] = a, R[a]++;
         return true;
